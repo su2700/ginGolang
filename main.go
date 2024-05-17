139c4a1df666b2025fd474e36f43b85ca0362c2d
N04h <noah.h.liu@gmail.com>
2024-05-17 20:42:56 +0200

    add REST

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
     r := gin.New()
-  
+    r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"msg": "pong.GET",
+		})
+	})
+	r.POST("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"msg": "pong.POST",
+		})
+	})
+	r.PUT()
+	r.DELETE()
     r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
